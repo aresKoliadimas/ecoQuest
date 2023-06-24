aba8ea8f5726088314e3dff2dda25002d55693d4
marvel <maribilisarri@gmail.com>
2023-06-24 20:42:42 +0300

    try fix tilemap2

--- ecoQuest.js
+++ ecoQuest.js
@@ -1,4 +1,5 @@
 import FirstLevel from "./first-level/first-level.js";
+import SecondLevel from "./first-level/second-level.js";
 import StartingScreen from "./starting-screen/starting-screen.js";
 
 const config = {
@@ -11,10 +12,11 @@ const config = {
       gravity: { y: 0 },
     },
   },
-  scene: [StartingScreen, FirstLevel],
+  scene: [SecondLevel],
   scale: {
     zoom: 1.4,
   },
 };
 
 export default new Phaser.Game(config);
+//StartingScreen, FirstLevel,
