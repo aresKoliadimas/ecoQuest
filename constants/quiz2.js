4a5ae6af898a265428d1404ccf602c0caa13701b
Ares Koliadimas <ariskoliadimas@gmail.com>
2023-06-25 17:38:17 +0300

    create quiz for 2nd level

---
+++ constants/quiz2.js
@@ -0,0 +1,51 @@
+export const QUESTIONS = [
+  {
+    question: "Which action helps prevent beach pollution?",
+    answers: {
+      correct: "Recycling",
+      wrong: ["Burning", "Dumping", "Littering"],
+    },
+  },
+  {
+    question:
+      "What is an alternative to single-use plastic items at the beach?",
+    answers: {
+      correct: "Reusable",
+      wrong: ["Disposable", "Plastic", "Non-recyclable"],
+    },
+  },
+  {
+    question: "What should be done with trash at the beach?",
+    answers: {
+      correct: "Dispose",
+      wrong: ["Leave", "Ignore", "Hide"],
+    },
+  },
+  {
+    question: "What is the main source of beach pollution?",
+    answers: {
+      correct: "Humans",
+      wrong: ["Animals", "Weather", "Plants"],
+    },
+  },
+  {
+    question: "What are the consequences of beach pollution?",
+    answers: {
+      correct: "Damage",
+      wrong: ["Enhancement", "Preservation", "Improvement"],
+    },
+  },
+];
+
+export const EXPLANATIONS = {
+  recycling:
+    "Recycling helps prevent beach pollution by reducing waste and promoting the reuse of materials.",
+  reusable:
+    "Using reusable items instead of single-use plastic items at the beach is a more sustainable choice that helps protect the environment.",
+  dispose:
+    "Trash at the beach should be properly disposed of in designated bins to keep the environment clean and prevent pollution.",
+  humans:
+    "Humans are the main source of beach pollution due to improper waste disposal and other human activities.",
+  damage:
+    "Beach pollution can lead to various negative consequences such as harm to marine life, degradation of ecosystems, and health risks for humans.",
+};
