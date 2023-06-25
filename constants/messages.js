e736bca18d31ca3955240551b1c4fa641e03fa06
Ares Koliadimas <ariskoliadimas@gmail.com>
2023-06-25 17:37:55 +0300

    update messages

--- constants/messages.js
+++ constants/messages.js
@@ -4,23 +4,33 @@ export const INSTRUCTIONS =
 export const BUILD_HOUSE =
   "You want to build the house. Select the material to use:\n\nWood\nRock\nConcrete";
 export const ROCK_UNSUSTAINABLE =
-  "Rock is unsustainable as a construction material due to environmental degradation during extraction, non-renewable nature, high energy consumption in transportation, and waste generation during processing. Adopting alternative materials and sustainable practices is crucial for a more environmentally friendly future.\n\nPress SPACE to continue";
+  "Rock is unsustainable as a construction material due to environmental degradation during extraction, non-renewable nature, high energy consumption in transportation, and waste generation during processing. Adopting alternative materials and sustainable practices is crucial for a more environmentally friendly future.\n\nPress SPACE to continue...";
 export const CONCRETE_UNSUSTAINABLE =
-  "Concrete is considered unsustainable due to several reasons. Firstly, its production requires large amounts of energy and emits significant carbon dioxide, contributing to greenhouse gas emissions and climate change. Additionally, the extraction of raw materials for concrete, such as limestone and sand, leads to habitat destruction and loss of biodiversity. Moreover, the production and use of concrete result in substantial water consumption, depleting water resources in many regions. Lastly, concrete structures have a limited lifespan and are often difficult to recycle, leading to significant waste generation. These combined factors make concrete an unsustainable solution in the context of environmental impact, resource depletion, and long-term sustainability.\n\nPress SPACE to continue";
+  "Concrete is considered unsustainable due to several reasons. Firstly, its production requires large amounts of energy and emits significant carbon dioxide, contributing to greenhouse gas emissions and climate change. Additionally, the extraction of raw materials for concrete, such as limestone and sand, leads to habitat destruction and loss of biodiversity. Moreover, the production and use of concrete result in substantial water consumption, depleting water resources in many regions. Lastly, concrete structures have a limited lifespan and are often difficult to recycle, leading to significant waste generation. These combined factors make concrete an unsustainable solution in the context of environmental impact, resource depletion, and long-term sustainability.\n\nPress SPACE to continue...";
 export const CUT_TREES =
-  "Wood is an excellent choice for construction due to its renewable nature, thermal insulation, lightweight properties, versatility, aesthetic appeal, resilience to earthquakes and extreme weather, and long lifespan. It is a sustainable, durable, and versatile material for building projects. However we need to respect the environment and its resources, so to minimize the negative impacts and promote sustainable practices reforestation is needed.\n\nCut 10 trees to build the house!\n\nPress SPACE to continue";
+  "Wood is an excellent choice for construction due to its renewable nature, thermal insulation, lightweight properties, versatility, aesthetic appeal, resilience to earthquakes and extreme weather, and long lifespan. It is a sustainable, durable, and versatile material for building projects. However we need to respect the environment and its resources, so to minimize the negative impacts and promote sustainable practices reforestation is needed.\n\nCut 10 trees to build the house!\n\nPress SPACE to continue...";
 export const MUST_BUILD_HOUSE =
-  "You must built the house. Go to the label...\n\nPress SPACE to continue";
+  "You must built the house. Go to the label...\n\nPress SPACE to continue...";
 export const CORRECT_ANSWER = "Yes! That's the correct answer.\n\n +50 points";
 export const WRONG_ANSWER = "Oops! That's not the correct answer.";
 export const CONGRATS =
-  "Congratulations! You answered all the questions correctly!\n\nPress SPACE to continue";
+  "Congratulations! You answered all the questions correctly!\n\nPress SPACE to continue...";
 
 export const POLLUTED_BEACH =
-  "You just arrived at the beach and saw a lot of garbages. What should you do:\nClean\nLeave polluted";
+  "You just arrived at the beach and saw a lot of garbages. How should you leave the beach?\n\nClean\nPolluted";
 export const CLEAN_BEACH =
-  " Beaches must be maintained clean for various reasons. Environmental Preservation,Marine Life Protection,Water Quality,Public Health and Safety,lastly Aesthetics and Enjoyment.\n\nSo you should clean the beach!\n\nPress SPACE to continue";
+  "Beaches must be maintained clean for various reasons. Environmental Preservation,Marine Life Protection,Water Quality,Public Health and Safety,lastly Aesthetics and Enjoyment.\n\nSo, you are correct, you should always clean the beach!\n\nPress SPACE to continue...";
 export const LEAVE_POLLUTED =
-  " Garbage thrown on the beach can pollute the water and soil. Moreover they can harm or disrupt the ecosystems. Marine animals, birds, and other wildlife can mistake the garbage for food or become entangled in it, leading to injury or death.\n\nPress SPACE to continue";
-export const MUST_CLEAN_BEACH =
-  "You must clean the beach. Go to the recycle bin...\n\nPress SPACE to continue";
+  "Garbage thrown on the beach can pollute the water and soil. Moreover they can harm or disrupt the ecosystems.\n\nMarine animals, birds, and other wildlife can mistake the garbage for food or become entangled in it, leading to injury or death.";
+export const PAPER_LEFT =
+  "Oops! Looks like there is still paper left at the beach. Don't forget to pick up all the litter, including paper, to keep the beach clean and beautiful.\n\nKeep up the good work and continue collecting the garbage!\n\nPress SPACE to continue...";
+export const PLASTIC_LEFT =
+  "Uh-oh! It seems that there is still plastic left on the beach. Plastic pollution is a serious problem, and we need to make sure to remove all the plastic waste to protect marine life and the environment.\n\nKeep up the effort and continue collecting the garbage to clean up the beach completely!\n\nPress SPACE to continue...";
+export const FOOD_LEFT =
+  "Oh no! There is still food waste scattered on the beach. Food waste can attract pests and create an unsightly environment. Let's make sure to clean up all the food waste and dispose of it properly. By doing so, we can help maintain a clean and healthy beach for everyone to enjoy.\n\nKeep up the good work!\n\nPress SPACE to continue...";
+export const GO_TO_BIN =
+  "Great job on collecting the trash! Now it's time to properly dispose of it. Head over to the nearby bin and throw the garbage away. By doing so, you'll help keep the environment clean and protect the wildlife that calls this place home. Remember, every small action counts in making a big difference. Thank you for taking responsibility and being a responsible player in our game!\n\nPress SPACE to continue...";
+export const GOOD_JOB =
+  "Congratulations! You have done an incredible job of collecting all the garbage and cleaning up the beach. Your efforts have made a significant impact in preserving the beauty of the beach and protecting the environment. By taking responsibility and actively participating in keeping our surroundings clean, you are setting a great example for others. Thank you for your dedication and commitment to creating a cleaner and healthier world.\n\nWell done!";
+export const BEAT_GAME =
+  "Congratulations! You have successfully completed all levels of the game and made a remarkable difference in the environment. Your dedication and efforts in cleaning both the town and the beach have had a significant impact on preserving the beauty of nature and promoting a cleaner, healthier world. By collecting all the garbage and answering the quiz questions correctly, you have shown exceptional commitment to environmental conservation. Thank you for being an environmental champion and playing a vital role in making our virtual world cleaner and greener. Well done on completing the game!\n\n";
