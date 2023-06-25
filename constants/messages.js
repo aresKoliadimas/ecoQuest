5236aafab415dffbb9670fdada88ee182d658248
marvel <maribilisarri@gmail.com>
2023-06-25 13:31:51 +0300

    add messages

--- constants/messages.js
+++ constants/messages.js
@@ -8,10 +8,20 @@ export const ROCK_UNSUSTAINABLE =
 export const CONCRETE_UNSUSTAINABLE =
   "Concrete is considered unsustainable due to several reasons. Firstly, its production requires large amounts of energy and emits significant carbon dioxide, contributing to greenhouse gas emissions and climate change. Additionally, the extraction of raw materials for concrete, such as limestone and sand, leads to habitat destruction and loss of biodiversity. Moreover, the production and use of concrete result in substantial water consumption, depleting water resources in many regions. Lastly, concrete structures have a limited lifespan and are often difficult to recycle, leading to significant waste generation. These combined factors make concrete an unsustainable solution in the context of environmental impact, resource depletion, and long-term sustainability.\n\nPress SPACE to continue";
 export const CUT_TREES =
-  "Wood is an excellent choice for construction due to its renewable nature, thermal insulation, lightweight properties, versatility, aesthetic appeal, resilience to earthquakes and extreme weather, and long lifespan. It is a sustainable, durable, and versatile material for building projects.\n\nCut 10 trees to build the house!\n\nPress SPACE to continue";
+  "Wood is an excellent choice for construction due to its renewable nature, thermal insulation, lightweight properties, versatility, aesthetic appeal, resilience to earthquakes and extreme weather, and long lifespan. It is a sustainable, durable, and versatile material for building projects. However we need to respect the environment and its resources, so to minimize the negative impacts and promote sustainable practices reforestation is needed.\n\nCut 10 trees to build the house!\n\nPress SPACE to continue";
 export const MUST_BUILD_HOUSE =
   "You must built the house. Go to the label...\n\nPress SPACE to continue";
 export const CORRECT_ANSWER = "Yes! That's the correct answer.\n\n +50 points";
 export const WRONG_ANSWER = "Oops! That's not the correct answer.";
 export const CONGRATS =
   "Congratulations! You answered all the questions correctly!\n\nPress SPACE to continue";
+
+export const  POLUTED_BEACH =
+"You just arrived at the beach and saw a lot of carbages. What sould you do:\n\Clean\nLeave_poluted";
+export const CLEAN_BEACH =
+  " Beaches must be maintained clean for various reasons. Environmental Preservation,Marine Life Protection,Water Quality,Public Health and Safety and Aesthetics and Enjoyment.\n\nSo you should clean the beach!\n\nPress SPACE to continue";
+export const  LEAVE_POLUTED =
+  " Garbage thrown on the beach can pollute the water and soil. Moreover they can harm or disrupt the ecosystems. Marine animals, birds, and other wildlife can mistake the garbage for food or become entangled in it, leading to injury or death.\n\nPress SPACE to continue";
+export const MUST_CLEAN_BEACH =
+  "You must clean the beach. Go to the recycle bin...\n\nPress SPACE to continue";
+  
